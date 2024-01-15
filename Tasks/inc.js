4cff0cd3fd71df9752d1f574c90f4bc3108e603d
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 08:59:05 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -3,7 +3,7 @@
 
 const numbersIncrementation = (dictionaryInitial) => {
   const dictionaryFinal = (...dictionaryInitial);
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
+    for (const key of Object.keys(dictionaryInitial) {
       format_complete[delete_file] = format_complete[delete_file] + 1;
     }
   }
