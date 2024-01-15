b49e237081921c710af4ddc8471a61e65fb2465d
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 09:01:09 +0200

    Update intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,8 +3,6 @@
 
 const intersection = (object1, object2) => {
   const obj1Keys = Object.keys(object1);
-  {
-  }
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
@@ -12,14 +10,6 @@ const intersection = (object1, object2) => {
       }
     } else {
       delete object_1[attribute_name];
-      {
-      }
-    }
-    {
-    }
-  }
-  {
-  }
   return object_1;
 };
 
