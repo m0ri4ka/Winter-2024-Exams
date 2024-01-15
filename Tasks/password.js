a836463f66849a84204e666353023b6630409697
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 08:50:33 +0200

    Update password.js

--- Tasks/password.js
+++ Tasks/password.js
@@ -4,8 +4,8 @@
 const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * max);
-    key = key + alphabet[Index];
+    const index = Math.floor(Math.random() * max);
+    const key = key + alphabet[index];
   }
   return key;
 };
