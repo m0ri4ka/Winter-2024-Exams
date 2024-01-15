018a9b8350e52bfabe00b63bbd0748bba60603a0
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 08:54:57 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,7 +3,7 @@
 
 const Day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const parseDay = (day) => {
   let i;
   for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
@@ -13,4 +13,4 @@ _parse_day_ = (s) => {
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
