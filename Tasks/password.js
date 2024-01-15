bbf6afb495487c509b142db2ab09812f9cc0663c
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 08:49:49 +0200

    Update password.js

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,7 +1,7 @@
 "use strict";
 // Generate random password
 
-let GeneratePassword = (alphabet, length) => {
+const generatePassword = (alphabet, length) => {
   const MAX = alphabet.length;
   key = '';
   for (let i = 0; i < length; i++) {
@@ -11,4 +11,4 @@ let GeneratePassword = (alphabet, length) => {
   return key;
 };
 
-module.exports = GeneratePassword;
+module.exports = generatePassword;
