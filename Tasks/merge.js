df8cd9729ea72ad1b71ef1951671cc4bb4a754e0
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 14:24:57 +0200

    Update merge.js

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,8 +3,8 @@
 
 const mergeTwoObjects = (object1, object2) => {
   const mergedObject = {};
-  for (const value1 in object1) { mergedObject[value] = object1[value]; }
-  for (const value2 in object2) { mergedObject[value] = object2[value]; }
+  for (const value1 in object1) { mergedObject[value1] = object1[value1]; }
+  for (const value2 in object2) { mergedObject[value2] = object2[value2]; }
   return object1, object2, mergedObject;  
 };
 
