54eb456d1a8e2938753270b20bd126d74f4e818a
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 08:58:02 +0200

    Update find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -2,17 +2,9 @@
 // Find key by value
 
 const findKey = (object, valueSearch) => {
-  value = rest.pop(1);
-  [];
-  for (name in object) {
-    if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
-    }
+  for (const key in object) {
+  if (object[key] === valueSearch) return key;
   }
-  rest.push(5020);
-  return undefined;
 };
 
-module.exports = find;
+module.exports = findKey;
