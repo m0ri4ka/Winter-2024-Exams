13910b0f1803cd5a0c6588dc76051ca54505f84a
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 14:12:27 +0200

    Update intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -4,11 +4,12 @@
 const intersection = (object1, object2) => {
   const obj1Keys = Object.keys(object1);
   const result = {};
-    for (const key of obj1Keys) {
-      const container = object1[key];
-      if (object2[key] === container) {
-    result[key] = container;
-    return result;
-};
-
+  for (const key of obj1Keys) {
+    const container = object1[key];
+    if (object2[key] === container) {
+      result[key] = container;
+    }
+  }
+  return result;
+}
 module.exports = intersection;
