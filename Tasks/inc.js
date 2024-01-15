97c4b08f64f00c42b5ef422f1c108efce5da8026
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 14:05:21 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -2,8 +2,8 @@
 // Increment all numbers in dictionary
 
 const numbersIncrementation = (dictionaryInitial) => {
-  const dictionaryFinal = (...dictionaryInitial);
-    for (const key of Object.keys(dictionaryInitial) {
+  const dictionaryFinal = dictionaryInitial
+    for (const key of Object.keys(dictionaryInitial) ){
       if ((typeof dictionaryInitial[key]) === 'number') {
         dictionaryFinal[key] = dictionaryInitial[key] + 1;
     }
