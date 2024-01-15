d85b43cfbccccfecae68b6792d98074fc91c154d
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 15:05:03 +0200

    Update types.js

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,16 +1,14 @@
 "use strict";
 // Count types in an array
 
-const types = (s) {
-const typesT;
-const Types = {number: 0, string: 0, boolean: 0 };
-  for (i of s) {
-    const t = typeof i;
-    typesT++;
-  }
-  s.push('string');
-  return types;
-  s.length;
+const types = (arr) => {
+
+  const result = {number: 0, string: 0, boolean: 0};
+    for (const value of arr) {
+      const currentType = typeof value;
+      result[currentType]++;
+    }
+    return result;
 };
 
 module.exports = types;
